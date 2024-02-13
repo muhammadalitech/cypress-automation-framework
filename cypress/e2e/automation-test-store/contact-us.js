4c9f7a31831fca1790bf297aff47d591545e3e77
ali <muhammadalirana263@gmail.com>
2024-02-13 11:05:42 +0500

    committing for selectors js file and chnages to update selectors

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -5,13 +5,17 @@ describe("Test Contact Us form via Automation Test store just for practicing", (
     cy.visit("https://automationteststore.com/");
     // for following we have used the cypress app, by using the slect the tool to generate selectors for us
     // in order to interact with them as developer may edit the HTML code for the given element 
-    cy.get('.info_links_footer > :nth-child(5) > a').click()
+   // cy.get('.info_links_footer > :nth-child(5) > a').click() // how we can improve it furthur if developer chnages the location of button later
+   cy.get("a[href$='contact']").click() 
     // for above we need to use a better approach
+    //a[contains(@href, 'contact')]
+    //cy.xpath("//a[contains(@href, 'contact')]").click()
     cy.get('#ContactUsFrm_first_name').type("Muhammad")
     cy.get('#ContactUsFrm_email').type("muhammmad.ali@uworx.co.uk")
     cy.get('#ContactUsFrm_enquiry').type("I have some questions to ask, do you provide additional discounts?")
-    cy.get('.col-md-6 > .btn').click()
-
+   // cy.get('.col-md-6 > .btn').click()   // how we can improve it, if a developer chnages its position later our test will fail to avoide this we will improve it
+   // improving our above line of code
+   cy.get("button[title='Submit']").click()
     });
 
 })
