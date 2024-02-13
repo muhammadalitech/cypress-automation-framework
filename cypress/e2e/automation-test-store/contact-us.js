f6b6af6c3fe32499e41cf5c250176a324e338b60
ali <muhammadalirana263@gmail.com>
2024-02-13 15:35:19 +0500

    updated in web driver uni contact Us file

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -12,10 +12,15 @@ describe("Test Contact Us form via Automation Test store just for practicing", (
     //cy.xpath("//a[contains(@href, 'contact')]").click()
     cy.get('#ContactUsFrm_first_name').type("Muhammad")
     cy.get('#ContactUsFrm_email').type("muhammmad.ali@uworx.co.uk")
+
+    cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email')
+
     cy.get('#ContactUsFrm_enquiry').type("I have some questions to ask, do you provide additional discounts?")
    // cy.get('.col-md-6 > .btn').click()   // how we can improve it, if a developer chnages its position later our test will fail to avoide this we will improve it
    // improving our above line of code
    cy.get("button[title='Submit']").click()
+   // following selector is selected using cypress tool
+   cy.get('.mb40 > :nth-child(3)').should('have.text','Your enquiry has been successfully sent to the store owner!')
     });
 
 })
--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -1,10 +1,33 @@
 ///  <reference types="Cypress"  />
 
-describe("Test Contact Us form via WebdriverUni", () =>{
-    it("Should be able tp submit a scuccessfull submission via contact us form", () => {
-// cypress code
-cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
-// cy.visit('https://www.automationteststore.com/');
+describe("Test Contact Us form via WebdriverUni", () => {
+    it.only("Should be able to submit a successful submission via contact us form", () => {
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        //cy.get('#contact-us').click({force: true})
+        cy.get('[name="first_name"]').type("Muhammad");
+        cy.get('[name="last_name"]').type("Ali");
+        cy.get('[name="email"]').type("muhammadalirana263@gmail.com")
+        cy.get('textarea.feedback-input').type("How can I learn Cypress from your platform?")
+        cy.get('[type="submit"]').click();
+        cy.get('h1').should('have.text', 'Thank You for your Message!')
+    });
+
+    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.get('[name="first_name"]').type("Muhammad");
+        cy.get('[name="last_name"]').type("Ali");
+        cy.get('textarea.feedback-input').type("How can I learn Cypress?")
+        cy.get('[type="submit"]').click();
+        cy.get('body').contains('Error: all fields are required');
+    });
+})
+
+
+
+//     it("Should be able tp submit a scuccessfull submission via contact us form", () => {
+// // cypress code
+// cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+// // cy.visit('https://www.automationteststore.com/');
 // cy.get("a[href$='contact']").click();
 //cy.get('#contact-us').click({force: true})
 
@@ -20,7 +43,7 @@ cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
 // cy.get("button[title='Submit']").click();
 
 
-    })
+    //})
 
 //     it.only("Should not be able tp submit a scuccessfull submission via contact us form as all fields are required ", () => {
 //         // cypress code
@@ -32,4 +55,4 @@ cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
         
 //             }) 
 
-})
+//})
